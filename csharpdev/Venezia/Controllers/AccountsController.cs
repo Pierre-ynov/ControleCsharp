0f819a0c44594e485c91a2b300abcee2063fccc3
Pierre VILLIERS <pierre.villiers@ynov.com>
2020-10-22 13:13:35 +0200

    implémentation d'un compte

--- csharpdev/Venezia/Controllers/AccountsController.cs
+++ csharpdev/Venezia/Controllers/AccountsController.cs
@@ -3,20 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Venezia.Data;
 using Venezia.Models;
 
 namespace Venezia.Controllers
 {
     public class AccountsController : Controller
     {
-        public IActionResult Index()
+        private readonly VeneziaContext _context;
+
+        public AccountsController(VeneziaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("login", Name = "Login")]
+        public IActionResult Login()
         {
             return View();
         }
 
-        public IActionResult Login()
+        [HttpPost]
+        [Route("login", Name = "Login")]
+        public IActionResult Login(string UserName, string PasswordHash)
         {
-            var account = new AccountUser {};
+            if (ModelState.IsValid)
+            {
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("register", Name = "Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("register", Name = "Register")]
+        public IActionResult Register([Bind("UserName","Email","PasswordHash")] AccountUser account)
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("logout", Name = "Logout")]
+        public IActionResult Logout()
+        {
             return View();
         }
     }
---
+++ csharpdev/Venezia/Migrations/20201022111244_delete_AccountRole.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Venezia.Migrations
+{
+    public partial class delete_AccountRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetRoles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetRoles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
